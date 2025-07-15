c31602dbc2e664a396f453b194d2e543b42be378
benjaminscheffe <benjaminscheffe@hotmail.com>
2025-07-15 16:05:09 +0200

    detail-view axios.get beat

--- src/components/buttonComponent/ButtonComponent.jsx
+++ src/components/buttonComponent/ButtonComponent.jsx
@@ -1,9 +1,9 @@
 import './ButtonComponent.scss';
 
-function ButtonComponent( {classNames, buttonText, noteIcon, downloadIcon, editIcon }) {
+function ButtonComponent( {classNames, buttonText, noteIcon, downloadIcon, editIcon, buttonFunction }) {
     console.log(classNames);
     return (
-        <button className={`btn ${classNames}`}>
+        <button className={`btn ${classNames}`} onClick={buttonFunction}>
             { buttonText }
             { noteIcon && <> <i className="fa-solid fa-music"></i></> }
             { downloadIcon && <> <i className="fa-solid fa-download"></i></> }
