6db8f046abf3d6a3e36dab2035a7c58d51b464c6
benjaminscheffe <benjaminscheffe@hotmail.com>
2025-07-15 17:02:41 +0200

    interim commit user axios.get information

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import LandingPage from "./pages/landingpage/Landingpage.jsx";
 import Header from "./components/header/Header.jsx";
 import ProductsOverview from "./pages/productsOverview/ProductsOverview.jsx";
 import ProductDetail from "./pages/productsOverview/productDetail/ProductDetail.jsx";
-import VisualTextBlock from "./components/visualTextBlock/VisualTextBlock.jsx";
 import LoginPage from "./pages/loginPage/LoginPage.jsx";
 import RegisterPage from "./pages/registerPage/RegisterPage.jsx";
 import NotFoundPage from "./pages/notFoundPage/NotFoundPage.jsx";
@@ -22,12 +21,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage/>} />
                 <Route path="/beats" element={<ProductsOverview/>} />
-                <Route path="/beats/1" element={<ProductDetail/>} />
-
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/register" element={<RegisterPage/>} />
                 <Route path="*" element={<NotFoundPage/>} />
-                <Route path="/user" element={<UserPage/>} />
+                <Route path="/user/:id" element={<UserPage/>} />
+                <Route path="/beats/:id" element={<ProductDetail/>} />
             </Routes>
 
         <footer>
