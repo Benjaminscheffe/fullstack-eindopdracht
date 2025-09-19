cb2a37fac9516fad159e25063bda56dd2dcff12a
benjaminscheffe <benjaminscheffe@hotmail.com>
2025-09-19 13:54:48 +0200

    BeatsOverview

--- src/pages/productsOverview/ProductsOverview.jsx
+++ src/pages/productsOverview/ProductsOverview.jsx
@@ -38,18 +38,18 @@ function ProductsOverview() {
     return (
         <main>
             <section className="main-content-block">
-                <div className="container">
+                <div className="container small-container">
                     <h1>Listen to all the beats</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </section>
             <section className="main-content-block no-padding-top">
-                <div className="container small-container">
+                <div className="container extra-small-container">
 
                     <div className="beats-container">
                         { beats.length > 0 ? beats.map((beat) =>
-                            <BeatBlock title={beat.title} artist={beat.userId} bpm={beat.bpm} price={beat.price} image={`http://localhost:8080/beats/${beat.id}/image`}>
-                                <ButtonComponent classNames="btn-small btn-border btnReset" buttonText="Go to the beat" noteIcon={true} buttonFunction={() => location.href=`/beats/${beat.id}`}  />
+                            <BeatBlock title={beat.title} artist={beat.userId} bpm={beat.bpm} price={beat.price} image={`http://localhost:8080/beats/${beat.id}/image`} error={error}>
+                                <ButtonComponent classNames="btn-small btn-border btn-inverted btnReset" buttonText="Go to the beat" noteIcon={true} buttonFunction={() => location.href=`/beats/${beat.id}`}  />
                             </BeatBlock>
                         ) : <p>Geen beats beschikbaar op dit moment</p>}
 
